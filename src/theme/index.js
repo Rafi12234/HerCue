314294ada33ed471eb55f80b93001544957828c0
webdevaustrc-2025 <shajidulkabir12345@gmail.com>
2026-09-06 02:58:08 +0600

    chore: add src/theme/index.js

---
+++ src/theme/index.js
@@ -0,0 +1,38 @@
+import { categoryColors, colors, gradients, statusColors } from './colors';
+import { duration, easing, pressScale, springs, stagger } from './motion';
+import { radii } from './radii';
+import { shadows } from './shadows';
+import { layout, spacing } from './spacing';
+import { fontFamily, typography } from './typography';
+
+export const theme = {
+  colors,
+  categoryColors,
+  statusColors,
+  gradients,
+  typography,
+  fontFamily,
+  spacing,
+  layout,
+  radii,
+  shadows,
+  motion: { duration, easing, springs, stagger, pressScale },
+};
+
+export {
+  categoryColors,
+  colors,
+  duration,
+  easing,
+  fontFamily,
+  gradients,
+  layout,
+  pressScale,
+  radii,
+  shadows,
+  spacing,
+  springs,
+  stagger,
+  statusColors,
+  typography,
+};
